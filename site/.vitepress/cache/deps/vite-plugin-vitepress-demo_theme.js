a2668a80aebf862ae15107fe3da4d98785f78a81
dargonc <dargonc@qq.com>
2023-03-02 15:42:44 +0800

    chore(vitepress): 优化vitepress站点配置

---
+++ site/.vitepress/cache/deps/vite-plugin-vitepress-demo_theme.js
@@ -0,0 +1,10 @@
+// node_modules/.pnpm/vite-plugin-vitepress-demo@2.0.0-alpha.8_fdor62hozdijozp3ymkhpxbnhi/node_modules/vite-plugin-vitepress-demo/dist/theme/index.mjs
+import AntdTheme from "/Users/shulong/ccproject/antd-tiny-vue/node_modules/.pnpm/vite-plugin-vitepress-demo@2.0.0-alpha.8_fdor62hozdijozp3ymkhpxbnhi/node_modules/vite-plugin-vitepress-demo/dist/theme/antd/index.vue";
+import DefaultTheme from "/Users/shulong/ccproject/antd-tiny-vue/node_modules/.pnpm/vite-plugin-vitepress-demo@2.0.0-alpha.8_fdor62hozdijozp3ymkhpxbnhi/node_modules/vite-plugin-vitepress-demo/dist/theme/default/index.vue";
+var theme_default = DefaultTheme;
+export {
+  AntdTheme,
+  DefaultTheme,
+  theme_default as default
+};
+//# sourceMappingURL=vite-plugin-vitepress-demo_theme.js.map
---
+++ site/components/button/index.md
@@ -0,0 +1,9 @@
+# button
+
+按钮
+
+## 演示例子
+
+::: deme 用法
+
+<demo src="../../demos/button/basic.vue"></demo>
